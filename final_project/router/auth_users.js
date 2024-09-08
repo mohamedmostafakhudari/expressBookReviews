87c6db72fec01f710b9959c4c1942b20c3e9c9a9
Mohamed Mostafa <mohamedmostafakhudari@gmail.com>
2024-09-08 22:44:04 +0300

    auth: validate and sign in a customer based on the username and password, saving credentials for the session as a JWT

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -15,12 +15,21 @@ const isValid = (username) => {
 const authenticatedUser = (username, password) => {
 	//returns boolean
 	//write code to check if username and password match the one we have in records.
+	return users.some((user) => user.username === username && user.password === password);
 };
-
 //only registered users can login
 regd_users.post("/login", (req, res) => {
 	//Write your code here
-	return res.status(300).json({ message: "Yet to be implemented" });
+	const { username, password } = req.body;
+	if (!username || !password) return res.status(404).json({ message: "cannot log in!" });
+
+	if (authenticatedUser(username, password)) {
+		const accessToken = jwt.sign({ password }, "access", { expiresIn: 60 * 60 });
+		req.session.authorization = { accessToken, username };
+		res.status(200).json({ message: `user ${username} has logged in successfully - ${Date.now()}` });
+	} else {
+		res.status(201).json({ message: "not registered user" });
+	}
 });
 
 // Add a book review
