32786fa087d2e17a8f68f3ef9e3c397c58c5b9bf
Mohamed Mostafa <mohamedmostafakhudari@gmail.com>
2024-09-08 20:07:02 +0300

    routing: get book reviews by its isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -58,7 +58,10 @@ public_users.get("/title/:title", function (req, res) {
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
 	//Write your code here
-	return res.status(300).json({ message: "Yet to be implemented" });
+	const { isbn } = req.params;
+	const book = books[isbn];
+
+	res.status(200).json({ ...book.reviews });
 });
 
 module.exports.general = public_users;
